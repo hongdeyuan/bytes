9b69f1e89a9dd1f17c378eec4139c08d332f8725
dehong <dehong@xsky.com>
2022-10-30 12:30:28 +0800

    publish v1.0.2 for @bytes/pro

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,55 +2,62 @@
  * Due to historical problems, the calculation process of TiB uses TB characters
  */
 declare const binaryMap: {
-    b: number;
-    kb: number;
-    mb: number;
-    gb: number;
-    tb: number;
-    pb: number;
-    eb: number;
-    zb: number;
+  b: number;
+  kb: number;
+  mb: number;
+  gb: number;
+  tb: number;
+  pb: number;
+  eb: number;
+  zb: number;
 };
 declare const unitMap: {
-    B: string;
-    KB: string;
-    MB: string;
-    GB: string;
-    TB: string;
-    PB: string;
-    EB: string;
-    ZB: string;
+  B: string;
+  KB: string;
+  MB: string;
+  GB: string;
+  TB: string;
+  PB: string;
+  EB: string;
+  ZB: string;
 };
 export declare type BinaryMapKeyTypes = keyof typeof binaryMap;
 export declare type UnitMapKeyTypes = keyof typeof unitMap;
 export interface OptionsType {
-    capacityBase?: number;
-    convert?: boolean;
-    decimalPlaces?: number;
-    fixedDecimals?: boolean;
-    needUnit?: boolean;
-    splitUnit?: boolean;
-    unitSeparator?: string;
-    thousandsSeparator?: string;
-    toUnit?: UnitMapKeyTypes;
-    withoutFloat?: boolean;
+  capacityBase?: number;
+  convert?: boolean;
+  decimalPlaces?: number;
+  fixedDecimals?: boolean;
+  needUnit?: boolean;
+  splitUnit?: boolean;
+  unitSeparator?: string;
+  thousandsSeparator?: string;
+  toUnit?: UnitMapKeyTypes;
+  withoutFloat?: boolean;
 }
 interface OptionsTypeWithSplitUnit extends OptionsType {
-    splitUnit?: true;
+  splitUnit?: true;
 }
 interface OptionsTypeWithOutSplitUnit extends OptionsType {
-    splitUnit?: false;
+  splitUnit?: false;
 }
 declare function format(value: number, options?: OptionsTypeWithOutSplitUnit): string | null;
 declare function format(value: number, options?: OptionsTypeWithSplitUnit): string[] | null;
-declare function parse(val: string, options?: {
+declare function parse(
+  val: string,
+  options?: {
     capacityBase?: number;
     convert?: boolean;
-}): number | null;
-declare function parseToUnit(value: string, toUnit: UnitMapKeyTypes, capacityBase?: 1024 | 1000): string | null;
+  },
+): number | null;
+declare function parseToUnit(
+  value: string,
+  toUnit: UnitMapKeyTypes,
+  capacityBase?: 1024 | 1000,
+): string | null;
 declare const bytes: {
-    format: typeof format;
-    parse: typeof parse;
-    parseToUnit: typeof parseToUnit;
+  format: typeof format;
+  parse: typeof parse;
+  parseToUnit: typeof parseToUnit;
 };
 export default bytes;
